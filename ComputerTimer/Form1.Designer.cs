422e56255f3583049b199221c8df1f1c57d4ce84
Nicaa <nica1408@gmail.com>
2018-02-25 13:35:31 +0100

    Init

---
+++ ComputerTimer/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ComputerTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SleepBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.HourTxt = new System.Windows.Forms.TextBox();
+            this.MinTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.AbortBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SleepBtn
+            // 
+            this.SleepBtn.Location = new System.Drawing.Point(89, 39);
+            this.SleepBtn.Name = "SleepBtn";
+            this.SleepBtn.Size = new System.Drawing.Size(46, 23);
+            this.SleepBtn.TabIndex = 0;
+            this.SleepBtn.Text = "OK";
+            this.SleepBtn.UseVisualStyleBackColor = true;
+            this.SleepBtn.Click += new System.EventHandler(this.SleepBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Values press OK to shutdown";
+            // 
+            // HourTxt
+            // 
+            this.HourTxt.Location = new System.Drawing.Point(7, 39);
+            this.HourTxt.Name = "HourTxt";
+            this.HourTxt.Size = new System.Drawing.Size(25, 20);
+            this.HourTxt.TabIndex = 2;
+            // 
+            // MinTxt
+            // 
+            this.MinTxt.Location = new System.Drawing.Point(47, 39);
+            this.MinTxt.Name = "MinTxt";
+            this.MinTxt.Size = new System.Drawing.Size(25, 20);
+            this.MinTxt.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "HH";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "MM";
+            // 
+            // AbortBtn
+            // 
+            this.AbortBtn.Location = new System.Drawing.Point(158, 39);
+            this.AbortBtn.Name = "AbortBtn";
+            this.AbortBtn.Size = new System.Drawing.Size(75, 23);
+            this.AbortBtn.TabIndex = 6;
+            this.AbortBtn.Text = "Abort";
+            this.AbortBtn.UseVisualStyleBackColor = true;
+            this.AbortBtn.Click += new System.EventHandler(this.AbortBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(236, 89);
+            this.Controls.Add(this.AbortBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.MinTxt);
+            this.Controls.Add(this.HourTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SleepBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Computer Timer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SleepBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox HourTxt;
+        private System.Windows.Forms.TextBox MinTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button AbortBtn;
+    }
+}
+
---
+++ ComputerTimer/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputerTimer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void SleepBtn_Click(object sender, EventArgs e)
+        {
+            int hours = 0;
+            int minutes = 0;
+
+            if (!string.IsNullOrEmpty(HourTxt.Text))
+            {
+                hours = convertToInt(HourTxt.Text);
+            }
+
+            if (!string.IsNullOrEmpty(MinTxt.Text))
+            {
+                minutes = convertToInt(MinTxt.Text);
+            }
+
+            TimeSpan span = new TimeSpan(hours, minutes, 0);
+
+            int seconds = (int)span.TotalSeconds;
+
+            
+
+            if (seconds <= 0)
+            {
+                DialogResult dialogResult = MessageBox.Show("Second = 0 this will shutdown computer now? \nAre you sure your want to shutdown now?", "Shutdown now", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Process.Start("shutdown", "/s /t " + seconds);
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    return;
+                }
+            }
+            else
+            {
+                Process.Start("shutdown", "/s /t " + seconds);
+            }
+
+
+        }
+
+        private int convertToInt(string value)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(value, "^[0-9]*$"))
+            {
+                return Int32.Parse(value);
+            }
+
+            MessageBox.Show("Please only enter integers!");
+            return 0;
+        }
+
+
+        private void AbortBtn_Click(object sender, EventArgs e)
+        {
+            Process.Start("shutdown", "/a");
+        }
+    }
+}
